1dcabbd217c05ebf09bf0257322901831c35d6f4
Ribhu Bhushan Tiwari <ribhubhushan08@gmail.com>
2026-09-15 13:07:55 +0530

    feat: add loan repository

---
+++ src/main/java/com/library/loan/LoanRepository.java
@@ -0,0 +1,9 @@
+package com.library.loan;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface LoanRepository extends JpaRepository<Loan, Long> {
+    List<Loan> findByStatus(LoanStatus status);
+}
